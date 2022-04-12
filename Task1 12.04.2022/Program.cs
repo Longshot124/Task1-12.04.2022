854b864fee5952e3942d5c1a908f984e2448b1be
kamra <kamra@LAPTOP-M49P7DNF>
2022-04-13 01:57:31 +0400

    Добавьте файлы проекта.

---
+++ Task1 12.04.2022/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task1_12._04._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("Reqem daxil edin");
+            int num = Convert.ToInt16(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Reqem cuttur");
+            }
+            else
+            {
+                Console.WriteLine("Reqem tekdir");
+            }
+
+            
+
+        }
+    }
+}
